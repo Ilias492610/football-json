7887cf1c75c88c7dd1eced8816231fab085ef86e
Ilias Hamdaoui <146335203+Ilias492610@users.noreply.github.com>
2025-05-23 22:34:14 +0200

    Add Render compatibility fixes

---
+++ dist/src/app.js
@@ -0,0 +1,4 @@
+// Dit bestand is gemaakt om compatibel te zijn met de Render deployment configuratie
+// Het importeert en voert het echte entry point van de applicatie uit
+
+require('../index.js');
